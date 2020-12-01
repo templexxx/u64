07484b9b3cb4d9a4c9e815888ccea3640375db4b
templexxx <lucas1x1x@gmail.com>
2020-12-01 11:08:16 +0800

    add atomic256 detection (> skylake)

---
+++ atomic256.go
@@ -0,0 +1,21 @@
+package u64
+
+import "github.com/templexxx/cpu"
+
+var isAtomic256 = false
+
+var isAtomic256CPUs = map[string]struct{}{
+	"06_4EH": {}, "06_5EH": {},
+	"06_55H": {},
+	"06_6AH": {}, "06_6CH": {},
+	"06_8EH": {}, "06_9EH": {},
+	"06_66H": {},
+	"06_A5H": {}, "06_A6H": {},
+	"06_7DH": {}, "06_7EH": {},
+}
+
+func init() {
+	if _, ok := isAtomic256CPUs[cpu.X86.Signature]; ok {
+		isAtomic256 = true
+	}
+}
