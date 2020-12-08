726670c5d20e072082ec5839ab1b20334b09a342
templexxx <lucas1x1x@gmail.com>
2020-12-09 07:26:24 +0800

    farm32 may slow down a bit, but has better properties

--- set.go
+++ set.go
@@ -136,6 +136,8 @@ func (s *Set) Add(key uint64) error {
 			s.unlock()
 			return ErrAddTooFast
 		}
+
+		// Try to expand.
 		idx := s.getWritableTable()
 		p := atomic.LoadPointer(&s.cycle[idx])
 		tbl := *(*[]uint64)(p)
@@ -308,7 +310,44 @@ var (
 // TODO check escape
 // hash function for cycle[0]
 var hashFunc0 = func(k uint64) uint64 {
-	return xxh3.HashU64(k, 0) // xxh3 is prefect bijective for 8bytes and blazing fast.
+	return uint64(farm32(k, 0))
+	//return xxh3.HashU64(k, 0) // xxh3 is prefect bijective for 8bytes and blazing fast.
+}
+
+func farm32(fid uint64, seed uint32) uint32 {
+	var a, b, c, d uint32
+	a = 8
+	b = 40
+	c = 9
+	d = b + seed
+	a += uint32(fid << 32 >> 32)
+	b += uint32(fid >> 32)
+	c += uint32(fid >> 32)
+	return fmix(seed ^ mur(c, mur(b, mur(a, d))))
+}
+
+// Magic numbers for 32-bit hashing.  Copied from Murmur3.
+const c1 uint32 = 0xcc9e2d51
+const c2 uint32 = 0x1b873593
+
+func mur(a, h uint32) uint32 {
+	// Helper from Murmur3 for combining two 32-bit values.
+	a *= c1
+	a = bits.RotateLeft32(a, -17)
+	a *= c2
+	h ^= a
+	h = bits.RotateLeft32(h, -19)
+	return h*5 + 0xe6546b64
+}
+
+// A 32-bit to 32-bit integer hash copied from Murmur3.
+func fmix(h uint32) uint32 {
+	h ^= h >> 16
+	h *= 0x85ebca6b
+	h ^= h >> 13
+	h *= 0xc2b2ae35
+	h ^= h >> 16
+	return h
 }
 
 // hash function for cycle[1]
--- set_prop_test.go
+++ set_prop_test.go
@@ -1,3 +1,19 @@
 package u64
 
+import "testing"
+
 // TODO when reach the first full
+func TestMitFull(t *testing.T) {
+	n := 1024 * 1024 * 4
+	s := New(n)
+	s.scale() // Forbidden expanding.
+	cnt := 0
+	for i := 0; i < n; i++ {
+		err := s.Add(uint64(i))
+		if err == ErrAddTooFast {
+			t.Logf("total: %d, first full: %d, load factor: %.2f", n, cnt, float64(cnt)/float64(n))
+			break
+		}
+		cnt++
+	}
+}
