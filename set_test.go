4117fb595ca622d5753cfb2bbad6030872088107
templexxx <lucas1x1x@gmail.com>
2020-12-26 21:52:42 +0800

    add update concurrent test & fix contains test fail in race detector

--- set_test.go
+++ set_test.go
@@ -1,6 +1,7 @@
 package u64
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -24,23 +25,32 @@ func TestSet_Contains(t *testing.T) {
 	for n := start; n <= MaxCap; n *= 32 {
 		keys := generateKeys(n/2, randomKey)
 		s := New(n)
-		for _, key := range keys {
-			err := s.Add(key)
-			if err != nil {
-				t.Fatal(err)
+
+		wg := new(sync.WaitGroup) // Using sync.WaitGroup for ensuring the order.
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, key := range keys {
+				err := s.Add(key)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !s.Contains(key) {
+					t.Fatal("should have key")
+				}
 			}
-			if !s.Contains(key) {
-				t.Fatal("should have key")
-			}
-		}
-		for _, key := range keys {
-			if !s.Contains(key) {
-				t.Fatal("should have key")
-			}
-		}
+		}()
+		wg.Wait()
+
 		_, usage := s.GetUsage()
 		if usage != len(keys) {
-			t.Fatal("usage size mismatched")
+			t.Fatal("usage mismatched")
+		}
+
+		for _, key := range keys {
+			if !s.Contains(key) {
+				t.Fatal("should have key")
+			}
 		}
 	}
 }
@@ -73,6 +83,49 @@ func TestSet_Remove(t *testing.T) {
 	}
 }
 
+// Add & Remove concurrently, checking dead lock or not.
+func TestSet_UpdateConcurrent(t *testing.T) {
+
+	n := 1024 * 4
+	s := New(n)
+	for i := 0; i < 1024; i++ {
+		err := s.Add(uint64(i))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 1024; i < 1024*2; i++ {
+			err := s.Add(uint64(i))
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1024; i++ {
+			s.Remove(uint64(i))
+		}
+	}()
+	wg.Wait()
+
+	_, usage := s.GetUsage()
+	if usage != 1024 {
+		t.Fatal("usage mismatched")
+	}
+
+	for i := 0; i < 1024; i++ {
+		if s.Contains(uint64(i)) {
+			t.Fatal("should not have key")
+		}
+	}
+}
+
 // TODO test
 // 1. concurrent range (sync.Map)
-// 2. concurrent add delete, will dead lock?
+// 2. expand background
